e4e4f8c73dcc34604d88ec07a1676c4318ca5181
Alejandra Lopez <alelopezbalboa@gmail.com>
2020-11-06 00:58:49 -0300

    borradas queries innecesarias

--- back-end/graphql-api/schemas/resolvers.js
+++ back-end/graphql-api/schemas/resolvers.js
@@ -4,25 +4,25 @@ const { Date } = require("./customScalars");
 const resolvers = {
     Date,
 	Query: {
-        persona: async (_, args) => {
-            const query = `SELECT * FROM Persona WHERE idpersona = $1`;
-            const values = [args.idpersona];
-            try {
-                const res = await db.oneOrNone(query, values);
-                return res;
-            } catch (err) {
-                return err;
-            }
-        },
-        personas: async () => {
-            const query = `SELECT * FROM Persona`;
-            try {
-                const res = await db.manyOrNone(query);
-                return res;
-            } catch (err) {
-                return err;
-            }
-        },
+        // persona: async (_, args) => {
+        //     const query = `SELECT * FROM Persona WHERE idpersona = $1`;
+        //     const values = [args.idpersona];
+        //     try {
+        //         const res = await db.oneOrNone(query, values);
+        //         return res;
+        //     } catch (err) {
+        //         return err;
+        //     }
+        // },
+        // personas: async () => {
+        //     const query = `SELECT * FROM Persona`;
+        //     try {
+        //         const res = await db.manyOrNone(query);
+        //         return res;
+        //     } catch (err) {
+        //         return err;
+        //     }
+        // },
         legisladores: async (_, args) => {
             const query = `SELECT * FROM persona_por_partido_y_periodo 
                             JOIN partido_por_periodo ON persona_por_partido_y_periodo.idpartidoporperiodo = partido_por_periodo.idpartidoporperiodo 
@@ -36,55 +36,55 @@ const resolvers = {
                 return err;
             }
         },
-        personasPorCargo: async (_, args) => {
-            const query = `SELECT * FROM Persona WHERE cargo = $1`;
-            const values = [args.cargo];
-            try {
-                const res = await db.manyOrNone(query, values);
-                return res;
-            } catch (err) {
-                return err;
-            }
-        },
-        personasPorTipo: async (_, args) => {
-            const query = `SELECT * FROM Persona WHERE tipo = $1`;
-            const values = [args.tipo];
-            try {
-                const res = await db.manyOrNone(query, values);
-                return res;
-            } catch (err) {
-                return err;
-            }
-        },
-        periodosLegislativos: async () => {
-            const query = `SELECT * FROM Periodo_Legislativo`;
-            try {
-                const res = await db.manyOrNone(query);
-                return res;
-            } catch (err) {
-                return err;
-            }
-        },
-        periodoLegislativo: async (_, args) => {
-            const query = `SELECT * FROM periodo_legislativo WHERE periodo_legislativo = $1`;
-            const values = [args.periodo_legislativo];
-            try {
-                const res = await db.one(query, values);
-                return res;
-            } catch (err) {
-                return err;
-            }
-        },
-        comision: async (_, args) => {
-            const query = `SELECT * FROM comision WHERE idcomision = $1`;
-            const values = [args.idcomision];
-            try {
-                const res = await db.one(query, values);
-                return res;
-            } catch (err) {
-                return err;
-            }
-        },
+        // personasPorCargo: async (_, args) => {
+        //     const query = `SELECT * FROM Persona WHERE cargo = $1`;
+        //     const values = [args.cargo];
+        //     try {
+        //         const res = await db.manyOrNone(query, values);
+        //         return res;
+        //     } catch (err) {
+        //         return err;
+        //     }
+        // },
+        // personasPorTipo: async (_, args) => {
+        //     const query = `SELECT * FROM Persona WHERE tipo = $1`;
+        //     const values = [args.tipo];
+        //     try {
+        //         const res = await db.manyOrNone(query, values);
+        //         return res;
+        //     } catch (err) {
+        //         return err;
+        //     }
+        // },
+        // periodosLegislativos: async () => {
+        //     const query = `SELECT * FROM Periodo_Legislativo`;
+        //     try {
+        //         const res = await db.manyOrNone(query);
+        //         return res;
+        //     } catch (err) {
+        //         return err;
+        //     }
+        // },
+        // periodoLegislativo: async (_, args) => {
+        //     const query = `SELECT * FROM periodo_legislativo WHERE periodo_legislativo = $1`;
+        //     const values = [args.periodo_legislativo];
+        //     try {
+        //         const res = await db.one(query, values);
+        //         return res;
+        //     } catch (err) {
+        //         return err;
+        //     }
+        // },
+        // comision: async (_, args) => {
+        //     const query = `SELECT * FROM comision WHERE idcomision = $1`;
+        //     const values = [args.idcomision];
+        //     try {
+        //         const res = await db.one(query, values);
+        //         return res;
+        //     } catch (err) {
+        //         return err;
+        //     }
+        // },
         sesionesPorAno: async (_, args) => {
             const query = `SELECT * FROM sesion WHERE extract(year from fecha) = $1`;
             const values = [args.ano];
@@ -105,18 +105,27 @@ const resolvers = {
                 return err;
             }
         },
-        sesionesPorComisionYPeriodo: async (_, args) => {
-            const query = `SELECT * FROM sesion WHERE idcomision = $1 AND periodo_legislativo = $2`;
-            const values = [args.idcomision, args.periodo_legislativo];
-            try {
-                const res = await db.manyOrNone(query, values);
-                return res;
-            } catch (err) {
-                return err;
-            }
-        },
-        partidoPorPeriodo: async () => {
-            const query = `SELECT * FROM partido_por_periodo`;
+        // sesionesPorComisionYPeriodo: async (_, args) => {
+        //     const query = `SELECT * FROM sesion WHERE idcomision = $1 AND periodo_legislativo = $2`;
+        //     const values = [args.idcomision, args.periodo_legislativo];
+        //     try {
+        //         const res = await db.manyOrNone(query, values);
+        //         return res;
+        //     } catch (err) {
+        //         return err;
+        //     }
+        // },
+        // partidoPorPeriodo: async () => {
+        //     const query = `SELECT * FROM partido_por_periodo`;
+        //     try {
+        //         const res = await db.manyOrNone(query);
+        //         return res;
+        //     } catch (err) {
+        //         return err;
+        //     }
+        // },
+        personaPorPartidoYPeriodo: async () => {
+            const query = `SELECT * FROM persona_por_partido_y_periodo`;
             try {
                 const res = await db.manyOrNone(query);
                 return res;
@@ -124,8 +133,8 @@ const resolvers = {
                 return err;
             }
         },
-        personaPorPartidoYPeriodo: async () => {
-            const query = `SELECT * FROM persona_por_partido_y_periodo`;
+        comisiones: async () => {
+            const query = `SELECT * FROM comision ORDER BY nombre`;
             try {
                 const res = await db.manyOrNone(query);
                 return res;
@@ -143,7 +152,7 @@ const resolvers = {
             } catch (err) {
                 return err;
             }
-        },
+        }
     },
     Sesion: {
         comision(parent) {
--- back-end/graphql-api/schemas/types.js
+++ back-end/graphql-api/schemas/types.js
@@ -5,19 +5,20 @@ const typeDefs = gql`
 	scalar Date
 
 	type Query {
-		persona(idpersona: ID!): Persona
-		personas: [Persona]
+		# persona(idpersona: ID!): Persona
+		# personas: [Persona]
 		legisladores(periodo: ID!): [Persona]
-		personasPorCargo(cargo: String!): [Persona]
-		personasPorTipo(tipo: String!): [Persona]
-		periodosLegislativos: [PeriodoLegislativo]
-		periodoLegislativo(periodo_legislativo: ID!): PeriodoLegislativo
-		comision(idcomision: ID!): Comision
+		# personasPorCargo(cargo: String!): [Persona]
+		# personasPorTipo(tipo: String!): [Persona]
+		# periodosLegislativos: [PeriodoLegislativo]
+		# periodoLegislativo(periodo_legislativo: ID!): PeriodoLegislativo
+		# comision(idcomision: ID!): Comision
 		sesionesPorAno(ano: Int!): [Sesion]
 		sesionesPorComisionYAno(idcomision: ID!, ano: Int!): [Sesion]
-		sesionesPorComisionYPeriodo(idcomision: ID!, periodo_legislativo: ID!): [Sesion]
-		partidoPorPeriodo: [PartidoPorPeriodo]
+		# sesionesPorComisionYPeriodo(idcomision: ID!, periodo_legislativo: ID!): [Sesion]
+		# partidoPorPeriodo: [PartidoPorPeriodo]
 		personaPorPartidoYPeriodo: [PersonaPorPartidoYPeriodo]
+		comisiones: [Comision]
 	}
 	
 	type Persona {
