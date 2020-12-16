a7ddebbdc97b561472689cabda5b5d50138d2762
Alejandra Lopez <alelopezbalboa@gmail.com>
2020-12-15 23:37:44 -0300

    home link arreglado

--- front_end/src/components/homeNav.js
+++ front_end/src/components/homeNav.js
@@ -1,15 +1,18 @@
-import React from "react"
+import React, { Fragment } from "react"
 import homeNavStyles from "./homeNav.module.css"
 import HomeIcon from '@material-ui/icons/Home';
 import { Link } from "gatsby"
 
 export default function HomeNav() {
     return (
-        <Link to="/">
-            <h1 className={homeNavStyles.homeNav}>
-                <HomeIcon fontSize="large" />
-                <div style={{marginLeft: `0.2em`}}>HOME</div>
-            </h1>
-        </Link>
+        <Fragment>
+            <Link to="/">
+                <h1 className={homeNavStyles.homeNav}>
+                    <HomeIcon fontSize="large" />
+                    <div style={{marginLeft: `0.2em`}}>HOME</div>
+                </h1>
+            </Link>
+            <div style={{width: `100%`}}></div>
+        </Fragment>
     )
 }
